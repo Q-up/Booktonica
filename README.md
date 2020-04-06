af617a7df6ca6240968b64f36c8a7f17f4194e3b
Quincey Hollman <qhollman@gmail.com>
2020-04-05 23:50:34 -0700

    working sort radio option buttons

--- README.md
+++ README.md
@@ -11,38 +11,6 @@
 - `npm install`
 - `npm start`
 
-## Requirements
-
-- Add a new DB column to store the name of the book's primary genre
-- Add more books - having more will help with this feature
-- Add a search/filter box at the top of the page under the existing header
-
-### Filtering / Sorting Toolbar (with 4 Widgets)
-
-These filters do not have to work at the same time (that is a challenge below). When a user chooses a filter, it can just "take over" as the only filter. Same goes for sorting. If you are sorting and choose a filter, it's okay if the sorting resets to the default.
-
-### Widget No.1 - Genre Filter | status: needs API endpoint
-
-Add a dropdown to filter by genre (at first, you can just hardcode a few genres)
-After it's working, commit and then get the items in the genre search dropdown from the database
-Add a new API endpoint that can give you all the distinct genres in the books table
-
-### Widget No. 2 - Sorting Radio Buttons | status: done | improvements: button stays clicked after selecting another
-
-Add a radio button to sort by newest publication date, oldest publication date, or title (A-Z). Newest pub date should be the starting sort.
-
-### Widget No. 3 - Reset Filters | status: done
-
-Add a button to reset all the filters and return the view to what it was when loading the page
-
-### Widget No. 4 Search Box
-
-I suggest to implement after everything else is working since it's the most complicated.
-
-Add a search text box with submit button. When submitting a search, you should only show books where the title includes the word(s) searched for. E.g. if you type "dog" it should match "Dog Walkers Guide" and "A Dog Day Afternoon." In formal terms, it's a case insensitive substring match.
-
-End of required features.
-
 ## Optional Challenges
 
 Add another filter options. They are so many possibilities
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -14,7 +14,38 @@ import BookCardList from "./components/BookCardList";
 import Filter from "./components/Filter";
 import Search from "./components/Search";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Form } from "reactstrap";
+
+//function generator to generate a sort function using a parameter and optional convertor
+//eg arrayOfObjs.sort(genSorter('foo', item => item.reverse))
+//equivalent to arrayOfObjs.sort((a, b) => a.foo.reverse > b.foo.reverse ? 1 : -1);
+function genSorter(param = null, converter = item => item) {
+  if (param) {
+    return (a, b) => (converter(a[param]) > converter(b[param]) ? 1 : -1);
+  } else {
+    return (a, b) => (converter(a) > converter(b) ? 1 : -1);
+  }
+}
+//to reverse the order of one of the generated sorters
+function reverse(sorter) {
+  return (a, b) => sorter(b, a);
+}
+
+const byDate = genSorter("publication_date", item => Date.parse(item));
+const byDateDesc = reverse(byDate);
+const byTitle = genSorter("title");
+const byTitleDesc = reverse(byTitle);
+
+/*
+const sortFunctions = {
+  'By Date': genSorter('publication_date', item => Date.parse(item)),
+  'By Title': genSorter('title'),
+  'By Title (Z->A)': reverse(genSorter('title', item => item)),
+  'By Author': genSorter('author'),
+  'By Author (Z->A)': reverse(genSorter('author')),
+  'By Genre': genSorter('genre'),
+  // ...
+}
+*/
 
 class App extends Component {
   constructor(props) {
@@ -38,20 +69,8 @@ class App extends Component {
     searchByBook(bookName).then(books => this.setState({ books: books }));
   };
 
-  filterByTitleAZ() {
-    const sortedBooks = this.state.books.sort((a, b) =>
-      a.title > b.title ? 1 : -1
-    );
-    this.setState({ books: sortedBooks });
-    console.log(sortedBooks);
-  }
-
-  filterByTitleZA() {
-    const sortedBooks = this.state.books.sort((a, b) =>
-      a.title < b.title ? 1 : -1
-    );
-    this.setState({ books: sortedBooks });
-    console.log(sortedBooks);
+  bookSorter(func) {
+    this.setState({ books: this.state.books.sort(func) });
   }
 
   filterByGenre(genre) {
@@ -61,6 +80,18 @@ class App extends Component {
   componentDidMount() {
     this.resetBookList();
     getAllGenres().then(genres => this.setState({ genres: genres }));
+  }
+
+  input(typeSort, label) {
+    return (
+      <InputGroup.Prepend>
+        <InputGroup.Radio
+          onClick={this.bookSorter.bind(this, typeSort)}
+          name='sortBook'
+        />
+        {label}
+      </InputGroup.Prepend>
+    );
   }
 
   render() {
@@ -75,22 +106,10 @@ class App extends Component {
             Reset
           </Button>
           <InputGroup>
-            <InputGroup.Prepend>
-              <InputGroup.Radio
-                onClick={this.filterByTitleAZ.bind(this)}
-                name='sortBook'
-              />
-              Sort by Title A-Z
-            </InputGroup.Prepend>
-          </InputGroup>
-          <InputGroup>
-            <InputGroup.Prepend>
-              <InputGroup.Radio
-                onClick={this.filterByTitleZA.bind(this)}
-                name='sortBook'
-              />
-              Sort by Title Z-A
-            </InputGroup.Prepend>
+            {this.input(byDateDesc, "Publication Date Newest")}
+            {this.input(byDate, "Publication Date Oldest")}
+            {this.input(byTitle, "Title A-Z")}
+            {this.input(byTitleDesc, "Title Z-A")}
           </InputGroup>
           <Search
             searchBookName={this.searchBookName}
